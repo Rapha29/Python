6510be1c7960c89d21ab4066ee347c702cc5f037
Gaby Perin <137819640+Gabyperin@users.noreply.github.com>
2023-09-02 00:30:03 -0300

    Perguntas para amigos com Python

---
+++ Perguntas para amigos/perguntas.py
@@ -0,0 +1,56 @@
+
+name = input("Digite seu nome ")
+print(f"Oiie {name}, espero que não se ofenda com as minhas perguntinhas :)")
+
+amigo = input("Qual o nome do seu melhor amigo no momento? ")
+print("Que fofos")
+
+resposta = input(f"{name},você gosta de programar? (Sim/Não) ")
+if resposta.lower() == "sim":
+    print("Você é PERFEITO(A) DE MAIS, (isso todo mundo já sabe)!")
+else:
+    print("Você é um baitolão!")
+
+animal_favorito= input(f"{name},qual é o seu animal favorito? ")
+print(f"Realmente, {animal_favorito}, parece muito com você mesmo! ")
+
+
+risadas_por_dia = input(f"Quantas vezes você ri por dia,{name}? ")
+risadas_por_dia = int(risadas_por_dia)
+if risadas_por_dia < 10:
+    print("Você precisa rir mais! Sua risada é tão gostosa!")
+else:
+    print("Você precisa rir mais ainda, sua risada é muito boa de ouvir!")
+
+bebida = input("Bebida favorita? ")
+print(f"Ai eu te pergunto,quando vamos tomar {bebida} juntos?")
+
+melhor_amigo = input(f"Você quer beijar o {amigo}? (Sim/Não) ")
+if melhor_amigo.lower() == "sim":
+    print("AAAIiHH eu shippo muito, que casalzãoo, quero ser madrinha, por favor!!")
+else:
+    print("Que feioo, mentindo para a sua amiga, eu shippo muito")
+
+
+fruta = input("Qual fruta você mais gosta? ")
+print(f" HUMMMM, {fruta}, tambem adoro essa fruta!")
+
+
+comida_favorita = input("Qual é a sua comida favorita? ")
+print(f"{comida_favorita}?? serioo ?? realmente voce sabe o que é comida boa!")
+
+
+gorduras = input(f"Quantos quilos voce tem,{name}? ")
+gorduras = int(gorduras)
+if risadas_por_dia <= 65:
+    print("Tá um pouquinho gordinho!")
+else:
+    print("Fecha essa boca ser humano, falo mesmo, ta gordoooo(a)!")
+
+gaby = input(f"{name}, gostou das minhas perguntas? (Sim/Não) ")
+if gaby.lower() == "sim":
+    print("Obrigadaaa,eu sou muita criativa")
+else:
+    print("Obrigada mesmo assim, até pq sua opniao é irrelevante kkkkkkkk")
+
+print("Obrigada por responder...@Gaby A.@")
