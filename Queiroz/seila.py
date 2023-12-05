86cbd3a1db9982d9c29d7f39b57575699ac2c53d
Rapha29 <122626805+Rapha29@users.noreply.github.com>
2023-12-05 10:36:46 -0300

    commit

--- Queiroz/seila.py
+++ Queiroz/seila.py
@@ -1,19 +1,14 @@
 import os
-import re
-import pandas as pd
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import time as tm
 import pyautogui as pg
-import xlrd
-import xlwt
 import pywhatkit as kit
-
-pg.PAUSE = 1
+import subprocess as sp
 
 # Caminho da pasta de downloads do navegador
-file_path = r'C:\Users\rapha\Downloads\relatorio_roteiro.xls'
+file_path = r'C:\Users\rapha\Downloads'
 
 # Definir endereço do site
 url = 'https://fhdistribuidora.tradepro.com.br/fhdistribuidora/login.jsf;jsessionid=8a607e2c07ea6d33f242c5bd7bce'
@@ -23,118 +18,112 @@ data_inicio = '01/11/2023'
 data_final = '15/11/2023'
 
 # Definir nomes a serem encontrados na planilha
-promotores = ['Allisson', 
-              'Daniel Silva', 
-              'Jocirley Paulo']
+promotores = ['Allisson',       # //*[@id="multSelectPromotor:multiSelectPromotor_panel"]/div[2]/ul/li[4]/label[1]
+              'Daniel Silva',   # //*[@id="multSelectPromotor:multiSelectPromotor_panel"]/div[2]/ul/li[6]/label[1]
+              'Jocirley Paulo'] # //*[@id="multSelectPromotor:multiSelectPromotor_panel"]/div[2]/ul/li[7]/label[1]
  
 # Definir numero do whatsapp para quem enviar cada relatório
 numero_whatsapp = "+559281529451"
 
-servico = Service(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=servico)
+# servico = Service(ChromeDriverManager().install())
+# navegador = webdriver.Chrome(service=servico)
 
-# Abre o navegador com o endereço selecionado
-navegador.get(url)
-pg.hotkey('winleft', 'up')
-tm.sleep(2)
+# # Abre o navegador com o endereço selecionado
+# navegador.get(url)
+# pg.hotkey('winleft', 'up')
+# tm.sleep(2)
 
-# Efetua o login no site
-navegador.find_element('xpath', '//*[@id="j_username"]').send_keys('') 
-navegador.find_element('xpath', '//*[@id="j_password"]').send_keys('')
-navegador.find_element('xpath', '//*[@id="botaoAcaoBancoEntrar"]/span[2]').click()
-navegador.find_element('xpath', '//*[@id="consulta"]/a').click()
-pg.scroll(-100)
+# # Efetua o login no site
+# navegador.find_element('xpath', '//*[@id="j_username"]').send_keys('Loraynne Sales') 
+# navegador.find_element('xpath', '//*[@id="j_password"]').send_keys('01031704Fh')
+# navegador.find_element('xpath', '//*[@id="botaoAcaoBancoEntrar"]/span[2]').click()
+# navegador.find_element('xpath', '//*[@id="consulta"]/a').click()
+# pg.scroll(-100)
 
-# Seleciona o link roteiros
-navegador.find_element('xpath', '//*[@id="roteiros"]/a').click()
-tm.sleep(1)
-pg.scroll(-200)
+# # Seleciona o link roteiros
+# navegador.find_element('xpath', '//*[@id="roteiros"]/a').click()
+# tm.sleep(1)
+# pg.scroll(-200)
 
-# Clica e modifica o texttobox de pesquisa entre datas
-navegador.find_element('xpath', '//*[@id="j_idt241"]/a').click()
-navegador.find_element('xpath', '//*[@id="dataInicial_input"]').click()
-pg.press('backspace', presses=10)
-tm.sleep(1)
+# # Clica e modifica o texttobox de pesquisa entre datas
+# navegador.find_element('xpath', '//*[@id="j_idt241"]/a').click()
+# navegador.find_element('xpath', '//*[@id="dataInicial_input"]').click()
+# pg.press('backspace', presses=10)
+# tm.sleep(1)
 
-# Define a Data inicial 
-navegador.find_element('xpath', '//*[@id="dataInicial_input"]').send_keys(data_inicio) 
-tm.sleep(1)
-pg.press('tab')
-pg.press('backspace', presses=10)
-tm.sleep(1)
+# def alterar_datas():
+#     # Define a Data inicial 
+#     elemento = navegador.find_element('xpath', '//*[@id="dataInicial_input"]')
+#     elemento.clear()
+#     elemento.send_keys(data_inicio)
+#     tm.sleep(1)
+#     pg.press('tab')
+#     pg.press('backspace', presses=10)
+#     # Define a Data final
+#     tm.sleep(2)
+#     elemento = navegador.find_element('xpath', '//*[@id="dataFinal_input"]')
+#     elemento.clear()
+#     elemento.send_keys(data_final)
+#     tm.sleep(2)
 
-# Define a Data final
-navegador.find_element('xpath', '//*[@id="dataFinal_input"]').send_keys(data_final)
-tm.sleep(2)
 
-# Clica em pesquisar para aplicar o filtro de datas
-navegador.find_element('xpath', '//*[@id="botaoAcaoBancoPesquisar"]').click()
-tm.sleep(5)
-
-# Baixa o arquivo excel
-navegador.find_element('xpath', '//*[@id="botaoAcaoBancoExcel_button"]').click()
-tm.sleep(2)
-
-#Tratando Planilha por nomes 
-book = xlrd.open_workbook(file_path)
-sheet = book.sheet_by_index(0)  # Assume que os dados estão na primeira planilha
-
-# Lendo o arquivo .xls e filtrando os dados para cada promotor
-dados_promotores = {promotor: [] for promotor in promotores}
-
-# Le o arquivo excel inicial para procurar todas as ocorrências dos nomes 
-for rowx in range(1, sheet.nrows):  # Ignorando o cabeçalho, começando da segunda linha
-    nome_na_coluna_b = sheet.cell_value(rowx, 1)  # Lendo a coluna 'B'
-    for promotor in promotores:
-        if nome_na_coluna_b == promotor:
-            row_values = [sheet.cell_value(rowx, colx) for colx in range(sheet.ncols)]
-            dados_promotores[promotor].append(row_values)
-
-# Salvando os dados em arquivos .xls separados com o nome do promotor, data inicial e data final
-diretorio_origem = os.path.dirname(file_path)
-caminhos_arquivos = []
-
-# Separa cada nome com suas respectivas entradas da planilha
-for promotor, dados in dados_promotores.items():
-    if dados:
-        novo_arquivo = xlwt.Workbook()
-        planilha = novo_arquivo.add_sheet('Sheet1')
-
-        for row_index, row in enumerate(dados):
-            for col_index, value in enumerate(row):
-                planilha.write(row_index, col_index, value)
-
-        # Criando o nome do arquivo evitando caracteres inválidos
-        nome_arquivo = f'{promotor}.xls'
-        nome_arquivo = re.sub(r'[^\w_.-]', '', nome_arquivo)
-
-        # Cria o caminho completo para salvar na mesma pasta do arquivo original com a extensão .xls
-        caminho_arquivo = os.path.join(diretorio_origem, nome_arquivo)
-        novo_arquivo.save(caminho_arquivo)
-        caminhos_arquivos.append(caminho_arquivo)
-        print(caminhos_arquivos)
+# for promotor in promotores:
+#     if promotor == "Allisson":
+#         navegador.find_element('xpath', '//*[@id="multSelectPromotor:multiSelectPromotor"]/div[3]/span').click()
+#         alterar_datas()
+#         tm.sleep(2)
+#         navegador.find_element('xpath', '//*[@id="multSelectPromotor:multiSelectPromotor_panel"]/div[2]/ul/li[4]/label[1]').click()
+#         tm.sleep(2)
+#         navegador.find_element('xpath', '//*[@id="botaoAcaoBancoPesquisar"]').click()
+#         tm.sleep(2)
+#         navegador.find_element('xpath', '//*[@id="botaoAcaoBancoExcel_button"]').click()
+#         tm.sleep(2)  
+#     elif promotor == "Daniel Silva":
+#         navegador.refresh()
+#         tm.sleep(2)     
+#         navegador.find_element('xpath', '//*[@id="multSelectPromotor:multiSelectPromotor"]/div[3]/span').click()
+#         alterar_datas()
+#         tm.sleep(2)
+#         navegador.find_element('xpath', '//*[@id="multSelectPromotor:multiSelectPromotor_panel"]/div[2]/ul/li[6]/label[1]').click()
+#         tm.sleep(2)
+#         navegador.find_element('xpath', '//*[@id="botaoAcaoBancoPesquisar"]').click()
+#         tm.sleep(2)
+#         navegador.find_element('xpath', '//*[@id="botaoAcaoBancoExcel_button"]').click()
+#         tm.sleep(2)     
+#     elif promotor == "Jocirley Paulo":
+#         navegador.refresh()
+#         tm.sleep(2)     
+#         navegador.find_element('xpath', '//*[@id="multSelectPromotor:multiSelectPromotor_label"]').click()
+#         alterar_datas()
+#         navegador.find_element('xpath', '//*[@id="multSelectPromotor:multiSelectPromotor_panel"]/div[2]/ul/li[7]/label[1]').click()
+#         tm.sleep(2)
+#         navegador.find_element('xpath', '//*[@id="botaoAcaoBancoPesquisar"]').click()
+#         tm.sleep(2)
+#         navegador.find_element('xpath', '//*[@id="botaoAcaoBancoExcel_button"]').click()
+#         tm.sleep(2)  
+#     else:
+#         break     
 
 # Envia cada arquivo para o número de WhatsApp especificado
-for promotor, caminho_arquivo in zip(promotores, caminhos_arquivos):
-    def enviar_arquivos_whatsapp(numero, caminhos_arquivos):
-        kit.sendwhatmsg_instantly(numero, f"Enviando planilha do intervalo de datas {data_inicio} até {data_final}...")
-        pg.press('enter')
-        tm.sleep(2)
-        pg.hotkey('shift', 'tab')
-        tm.sleep(1)
-        pg.press('space')
-        tm.sleep(1)
-        pg.press('down')
-        tm.sleep(1)
-        pg.press('space')
-        tm.sleep(1)
-        pg.write(caminho_arquivo)
-        tm.sleep(1)
-        pg.press('enter') 
-        tm.sleep(1)
-        pg.press('enter') 
-        tm.sleep(2)
-        pg.hotkey('control', 'w')
+def enviar_arquivos_whatsapp(numero):
+    kit.sendwhatmsg_instantly(numero, f"Enviando planilha do intervalo de datas {data_inicio} até {data_final}...")
+    sp.run(["explorer", file_path])
+    
+    # Define as coordenadas iniciais e finais para o movimento
+    x_inicial, y_inicial = 100, 100  # coordenadas iniciais
+    x_final, y_final = 300, 300      # coordenadas finais
 
-    # Envio dos arquivos
-    enviar_arquivos_whatsapp(numero_whatsapp, caminhos_arquivos)
+    # Move o cursor para a posição inicial
+    pg.moveTo(x_inicial, y_inicial)
+
+    # Clica e segura o botão do mouse
+    pg.mouseDown()
+
+    # Move o cursor para a posição final ao longo de alguns segundos
+    pg.moveTo(x_final, y_final, duration=2)
+
+    # Solta o botão do mouse
+    pg.mouseUp()
+
+# Envio dos arquivos
+enviar_arquivos_whatsapp(numero_whatsapp)
